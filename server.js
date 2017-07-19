e9b05c01854c859b558909f71aaa9c4aa7b47d14
System <none>
2017-07-19 16:04:05 +0000

    Checkpoint

---
+++ server.js
@@ -0,0 +1,52 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+/*
+app.get("/dreams", function (request, response) {
+  response.send(dreams);
+});
+
+// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+app.post("/dreams", function (request, response) {
+  dreams.push(request.query.dream);
+  response.sendStatus(200);
+});
+
+// Simple in-memory store for now
+var dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+];
+*/
+app.get("/api/whoami",callback);
+function callback(req,res){
+  var x={
+    "ipaddress": req.headers.['x-forwarded-for,
+    "language": req.headers.accept-language,
+    "software": req.headers.user-agent
+  }
+  console.log(req.headers);
+  // x-forwarded-for (ip address)
+  //accept-language(language)
+  // 'user-agent'
+  
+}
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
